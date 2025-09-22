3fe3324ecf6db20ee6446af93aa233332c553f66
cowalll <carl.j.ancheta@gmail.com>
2025-09-22 14:45:22 -0400

    Activity 1.4 modify example from Chapter 4

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime, timezone
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
@@ -13,6 +13,7 @@ Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = EmailField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,14 +21,19 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
     return render_template('index.html', 
                            current_time=datetime.now(timezone.utc),
                            form=form,
-                           name=session.get('name'))
+                           name=session.get('name'),
+                           email=session.get('email'))
 
 @app.route('/user/<name>')
 def user(name):
